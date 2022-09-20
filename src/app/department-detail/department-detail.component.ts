68cdeefa67e9edc552feb26c3ef35abb5f268879
Vuthik <kolvongvuthik@gmail.com>
2022-09-20 15:38:20 +1000

    tut26

--- src/app/department-detail/department-detail.component.ts
+++ src/app/department-detail/department-detail.component.ts
@@ -1,18 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router, ParamMap } from '@angular/router';
 @Component({
   selector: 'app-department-detail',
-  // templateUrl: './department-detail.component.html',
-  template: `<h3>You selected department with id = {{ departmentId }}</h3>`,
+  templateUrl: './department-detail.component.html',
   styles: [],
 })
 export class DepartmentDetailComponent implements OnInit {
   public departmentId: number;
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private router: Router) {}
 
   ngOnInit(): void {
     //get id
-    let id = parseInt(this.route.snapshot.paramMap.get('id') as string);
-    this.departmentId = id;
+    //snap shot approach
+    //gets parameter from route url
+    //drawback: angular reuses components if its same
+    //ngOnInit does not get called again so component doesn't get rerendered with right variables
+    // let id = parseInt(this.route.snapshot.paramMap.get('id') as string);
+    // this.departmentId = id;
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      let id = parseInt(params.get('id')!)
+      this.departmentId = id;
+    });
+  }
+  goPrevious() {
+    let previousId = this.departmentId - 1;
+    this.router.navigate(['/departments', previousId]);
+  }
+  goNext() {
+    let nextId = this.departmentId + 1;
+    this.router.navigate(['/departments', nextId]);
   }
 }
